eece3f0568f1245d76d5c1255e8d50e7c9040079
Evgeny Razumov <razumov.evgeni@gmail.com>
2016-10-07 11:42:13 +0300

    add tests for events: AgentConnect, AgentComplete, Bridge, Dial, FullyBooted, Join, Link

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -8,12 +8,153 @@ use React\Stream\Stream;
 
 class EventTest extends TestCase
 {
-    public function testListenEvent()
+    public function testEvents()
     {
-        $stream = $this->app[Stream::class];
-        Emitter::listen('ami.listen.started', function () use ($stream) {
-            $stream->emit('data', ["Asterisk Call Manager/1.3\r\n"]);
-            $stream->emit('data', ["Event: FullyBooted\r\nPrivilege: system,all\r\nStatus: Fully Booted\r\n\r\n"]);
+        $messages = [
+            [
+                'Event'          => 'AgentConnect',
+                'Privilege'      => 'agent,all',
+                'Queue'          => 'taxi-operators',
+                'Uniqueid'       => '1321511811.113',
+                'Channe'         => ' SIP/100-00000072',
+                'Member'         => 'SIP/100',
+                'MemberName'     => 'SIP/100',
+                'Holdtime'       => '10',
+                'BridgedChannel' => '1321511815.114',
+                'Ringtime'       => '9',
+            ],
+            [
+                'Event'      => 'AgentComplete',
+                'Privilege'  => 'agent,all',
+                'Queue'      => 'taxi-operators',
+                'Uniqueid'   => '1321511811.113',
+                'Channel'    => 'SIP/100-00000072',
+                'Member'     => 'SIP/100',
+                'MemberName' => 'SIP/100',
+                'HoldTime'   => '10',
+                'TalkTime'   => '7',
+                'Reason'     => 'caller',
+            ],
+            [
+                'Event'       => 'Bridge',
+                'Privilege'   => 'call,all',
+                'Bridgestate' => 'Link',
+                'Bridgetype'  => 'core',
+                'Channel1'    => 'SIP/mangotrunk-0000016c',
+                'Channel2'    => 'SIP/261-0000016d',
+                'Uniqueid1'   => '1324068645.605',
+                'Uniqueid2'   => '1324068650.606',
+                'Callerid1'   => '74997623634',
+                'Callerid2'   => '261',
+            ],
+            [
+                'Event'        => 'Dial',
+                'Privilege'    => 'call,all',
+                'Subevent'     => 'Begin',
+                'Channel'      => 'SIP/mangotrunk-0000016c',
+                'Destination'  => 'SIP/261-0000016d',
+                'Calleridnum'  => '74997623634',
+                'Calleridname' => '74997623634',
+                'Uniqueid'     => '1324068645.605',
+                'Destuniqueid' => '1324068650.606',
+                'Dialstring'   => '261',
+            ],
+            [
+                'Event'     => 'FullyBooted',
+                'Privilege' => 'system,all',
+                'Status'    => 'Fully Booted',
+            ],
+            [
+                'Event'             => 'Join',
+                'Privilege'         => 'call,all',
+                'Channel'           => 'SIP/multifon-out-00000071',
+                'CallerIDNum'       => '79265224173',
+                'CallerIDName'      => 'unknown',
+                'ConnectedLineNum'  => 'unknown',
+                'ConnectedLineName' => 'unknown',
+                'Queue'             => 'taxi-operators',
+                'Position'          => '1',
+                'Count'             => '1',
+                'Uniqueid'          => '1321511811.113',
+            ],
+            [
+                'Event'     => 'Link',
+                'Channel1'  => 'SIP/101-3f3f',
+                'Channel2'  => 'Zap/2-1',
+                'Uniqueid1' => '1094154427.10',
+                'Uniqueid2' => '1094154427.11',
+            ],
+        ];
+        Emitter::listen('ami.listen.started', function () use ($messages) {
+            $this->assertTrue(true);
+            $this->stream->emit('data', ["Asterisk Call Manager/1.3\r\n"]);
+            foreach ($messages as $lines) {
+                $message = '';
+                foreach ($lines as $key => $value) {
+                    $message .= "{$key}: {$value}\r\n";
+                }
+                $this->stream->emit('data', ["{$message}\r\n"]);
+            }
+        });
+        Emitter::listen('ami.events.agent_connect', function (Event $event) {
+            $this->assertEquals($event->getFields(), [
+                'Event'          => 'AgentConnect',
+                'Privilege'      => 'agent,all',
+                'Queue'          => 'taxi-operators',
+                'Uniqueid'       => '1321511811.113',
+                'Channe'         => ' SIP/100-00000072',
+                'Member'         => 'SIP/100',
+                'MemberName'     => 'SIP/100',
+                'Holdtime'       => '10',
+                'BridgedChannel' => '1321511815.114',
+                'Ringtime'       => '9',
+            ]);
+            $this->assertEquals($event->getName(), 'AgentConnect');
+        });
+        Emitter::listen('ami.events.agent_complete', function (Event $event) {
+            $this->assertEquals($event->getFields(), [
+                'Event'      => 'AgentComplete',
+                'Privilege'  => 'agent,all',
+                'Queue'      => 'taxi-operators',
+                'Uniqueid'   => '1321511811.113',
+                'Channel'    => 'SIP/100-00000072',
+                'Member'     => 'SIP/100',
+                'MemberName' => 'SIP/100',
+                'HoldTime'   => '10',
+                'TalkTime'   => '7',
+                'Reason'     => 'caller',
+            ]);
+            $this->assertEquals($event->getName(), 'AgentComplete');
+        });
+        Emitter::listen('ami.events.bridge', function (Event $event) {
+            $this->assertEquals($event->getFields(), [
+                'Event'       => 'Bridge',
+                'Privilege'   => 'call,all',
+                'Bridgestate' => 'Link',
+                'Bridgetype'  => 'core',
+                'Channel1'    => 'SIP/mangotrunk-0000016c',
+                'Channel2'    => 'SIP/261-0000016d',
+                'Uniqueid1'   => '1324068645.605',
+                'Uniqueid2'   => '1324068650.606',
+                'Callerid1'   => '74997623634',
+                'Callerid2'   => '261',
+            ]);
+            $this->assertEquals($event->getName(), 'Bridge');
+        });
+        Emitter::listen('ami.events.dial', function (Event $event) {
+            $this->assertEquals($event->getFields(), [
+                'Event'        => 'Dial',
+                'Privilege'    => 'call,all',
+                'Subevent'     => 'Begin',
+                'Channel'      => 'SIP/mangotrunk-0000016c',
+                'Destination'  => 'SIP/261-0000016d',
+                'Calleridnum'  => '74997623634',
+                'Calleridname' => '74997623634',
+                'Uniqueid'     => '1324068645.605',
+                'Destuniqueid' => '1324068650.606',
+                'Dialstring'   => '261',
+            ]);
+            $this->assertEquals($event->getName(), 'Dial');
         });
         Emitter::listen('ami.events.fully_booted', function (Event $event) {
             $this->assertEquals($event->getFields(), [
@@ -21,12 +162,36 @@ class EventTest extends TestCase
                 'Privilege' => 'system,all',
                 'Status'    => 'Fully Booted',
             ]);
+            $this->assertEquals($event->getName(), 'FullyBooted');
         });
-        $loop = $this->app['ami.eventloop'];
-        //stop after first tick
-        $loop->nextTick(function () use ($loop) {
-            $loop->stop();
+        Emitter::listen('ami.events.join', function (Event $event) {
+            $this->assertEquals($event->getFields(), [
+                'Event'             => 'Join',
+                'Privilege'         => 'call,all',
+                'Channel'           => 'SIP/multifon-out-00000071',
+                'CallerIDNum'       => '79265224173',
+                'CallerIDName'      => 'unknown',
+                'ConnectedLineNum'  => 'unknown',
+                'ConnectedLineName' => 'unknown',
+                'Queue'             => 'taxi-operators',
+                'Position'          => '1',
+                'Count'             => '1',
+                'Uniqueid'          => '1321511811.113',
+            ]);
+            $this->assertEquals($event->getName(), 'Join');
         });
+        Emitter::listen('ami.events.link', function (Event $event) {
+            $this->assertEquals($event->getFields(), [
+                'Event'     => 'Link',
+                'Channel1'  => 'SIP/101-3f3f',
+                'Channel2'  => 'Zap/2-1',
+                'Uniqueid1' => '1094154427.10',
+                'Uniqueid2' => '1094154427.11',
+            ]);
+            $this->assertEquals($event->getName(), 'Link');
+            $this->running = false;
+        });
+        $this->running = true;
         $this->artisan('ami:listen');
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,12 +2,48 @@
 
 namespace Enniel\Ami\Tests;
 
+use React\EventLoop\LoopInterface;
+use React\Stream\Stream;
+
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
+    /**
+     * @var \React\EventLoop\LoopInterface
+     */
+    protected $loop;
+
+    /**
+     * @var \React\Stream\Stream
+     */
+    protected $stream;
+
+    /**
+     * @var boolean
+     */
+    protected $running;
+
+    /**
+     * {@inheritdoc}
+     */
     protected function getPackageProviders($app)
     {
         return [
             \Enniel\Ami\Tests\AmiServiceProvider::class,
         ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->loop   = $this->app[LoopInterface::class];
+        $this->loop->nextTick(function () {
+            if (! $this->running) {
+                $this->loop->stop();
+            }
+        });
+        $this->stream = $this->app[Stream::class];
     }
 }
